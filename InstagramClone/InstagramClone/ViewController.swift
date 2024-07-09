e19e1d1d6d3015f83c4744fc2438fc2e99ae1993
JaeSunEo <wotjs020708@gmail.com>
2024-07-09 16:14:09 +0900

    스토리보드 삭제 및 기본 설정

---
+++ InstagramClone/InstagramClone/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  InstagramClone
+//
+//  Created by 어재선 on 7/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        title = "ViewController"
+        view.backgroundColor = .white
+    }
+
+
+}
+
