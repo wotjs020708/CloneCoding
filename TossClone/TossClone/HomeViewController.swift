907f8d698d81d1322417c4c0795fa15a6fa21bd3
JaeSunEo <wotjs020708@gmail.com>
2024-07-15 21:11:41 +0900

    토스앱 UI 클론 코딩을 위한 준비

---
+++ TossClone/TossClone/HomeViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  TossClone
+//
+//  Created by 어재선 on 7/15/24.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        
+        self.navigationController?.navigationBar.barTintColor = .white
+        self.navigationController?.title = "toss"
+        super.viewDidLoad()
+        self.view.backgroundColor = .gray
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
