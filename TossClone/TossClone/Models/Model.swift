5ea04374df7a081f9d369193278fc66f0b600791
JaeSunEo <wotjs020708@gmail.com>
2024-07-19 00:41:13 +0900

    더미 데이터 수정

--- TossClone/TossClone/Models/Model.swift
+++ TossClone/TossClone/Models/Model.swift
@@ -25,14 +25,13 @@ struct ListItem {
 
 struct ListSection{
     let items: [ListItem]
-    
     static func generateData() -> [ListSection] {
         return [
             ListSection(items: [ListItem(title: "토스뱅크", subTitle: nil, rightdetail: nil, type: .tossBank, imageName: nil)]),
             ListSection(items: [ListItem(title: "토스뱅크 통장", subTitle: "30,515", rightdetail: "송금", type: .account, imageName: "TossBank"),
                                 ListItem(title: "카카오뱅크 통장", subTitle: "0", rightdetail: "송금", type: .account, imageName: "KaKaoBank"),
-                                ListItem(title: "토스뱅크에 쌓인 이자", subTitle: "19", rightdetail: "지금 받기", type: .account, imageName: ""),
-                                ListItem(title: "IBK뱅크 통장", subTitle: "0", rightdetail: "송금", type: .account, imageName: "IBKBank"),]),
+                                ListItem(title: "토스뱅크에 쌓인 이자", subTitle: "19", rightdetail: "지금 받기", type: .account, imageName: nil),
+                                ListItem(title: "IBK뱅크 통장", subTitle: "0", rightdetail: "송금", type: .account, imageName: "IBKBank")]),
 //            ListSection(items: [ListItem(title: "7월에 쓴돈", subTitle: "0", rightdetail: "내역", type: .expenditure, imageName: "creditcard")])
         ]
     }
