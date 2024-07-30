9eadebed50f91b16eab8e8c64f958f030ab4904c
JaeSunEo <wotjs020708@gmail.com>
2024-07-30 09:15:42 +0900

    Initialize Project PhotoClone

---
+++ PhotoClone/PhotoClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoClone
+//
+//  Created by 어재선 on 7/27/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
